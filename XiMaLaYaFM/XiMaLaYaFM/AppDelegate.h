86c0f97148f9343fc8941b5cda0326a6852e63b2
archy <444163419@qq.com>
2017-02-22 12:00:07 +0800

    初始化项目

---
+++ XiMaLaYaFM/XiMaLaYaFM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XiMaLaYaFM
+//
+//  Created by archy on 17/2/22.
+//  Copyright © 2017年 archy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XiMaLaYaFM/XiMaLaYaFM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XiMaLaYaFM
+//
+//  Created by archy on 17/2/22.
+//  Copyright © 2017年 archy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
